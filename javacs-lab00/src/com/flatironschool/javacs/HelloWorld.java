0ff82e4bf647872b95a31fc63c3bbce2e58c20f1
Eric Musyoka <ericmutua100@gmail.com>
2016-03-19 20:54:48 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,19 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        //get java.version property as a string and convert it to double
+        //The first 3 characters should have the version number
+        try{
+        	String trimmedJavaVersion = System.getProperty("java.version").substring(0, 3);
+        	return Double.parseDouble(trimmedJavaVersion);
+        } catch(NumberFormatException e) {
+        	//Return negative number on failure
+        	return -1.0;
+        }   
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    //test getVersion
+	    System.out.println(getVersion());
     }
 }
